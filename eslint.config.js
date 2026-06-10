adfc0e44297a95f8275ec6c5275838cb6a08f3d2
Felipe Augusto <augusto.felipedev@gmail.com>
2026-06-10 13:59:56 -0300

    chore(config): add eslint, prettier, husky and commitlint

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  prettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', 'eslint.config.js', 'commitlint.config.js'],
+  },
+)
