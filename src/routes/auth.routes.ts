5d861d8808d5a0077a33cb62e9abbd18e10eb1c6
Felipe Augusto <augusto.felipedev@gmail.com>
2026-07-08 10:15:24 -0300

    fix(route): fix getMe api route

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -8,5 +8,5 @@ const router = Router()
 
 router.post('/register', validate(registerSchema), register)
 router.post('/login', validate(loginSchema), login)
-router.get('/auth/me', authenticate, getMe)
+router.get('/me', authenticate, getMe)
 export default router
